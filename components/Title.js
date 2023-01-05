2d3a0f930b8992b48c0fddb6434ba14da190386a
Ahmad <ahmadmuhammad.7700@gmail.com>
2023-01-05 22:40:09 +0500

    first commit

---
+++ components/Title.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Title = () => {
+  return (
+      <h2 className='text-white mt-2 text-center fixed top-0 left-0 right-0 bottom-0 z-[10]'>Weather App</h2>
+  )
+}
+
+export default Title
